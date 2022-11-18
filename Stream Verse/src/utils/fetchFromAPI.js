225925e0dcf0772a204b496c5da3449e332f542c
Shashank807 <109886612+Shashank807@users.noreply.github.com>
2022-11-18 15:55:13 +0530

    Add files via upload

---
+++ Stream Verse/src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  method: 'GET',
+  url: 'https://youtube-v31.p.rapidapi.com/captions',
+  params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
+  headers: {
+    'X-RapidAPI-Key': 'c0e45c224bmshf55ed3a370353e3p159527jsnfc20a2ac04a8',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
